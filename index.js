9a5158790e8f66502b49c6abf202dd8461e37ab2
walid-official <walid.official8@gmail.com>
2025-02-20 15:45:48 +0600

    server creation is done

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from TaskManager Server....");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
